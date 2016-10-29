d0af847a8c89ef248a12e495de20d72ccc91863b
Matthew Kweskin <matthew@kweskin.com>
2016-10-29 16:44:04 -0400

    Fix indentations

--- Programming-exercises/prefixes.py
+++ Programming-exercises/prefixes.py
@@ -12,7 +12,7 @@ for length in range(1,8):
         if (len(word) >= length) and (word[0:length] not in current_prefix):
             current_prefix.append(word[0:length])
     with open(str(length)+"prefix", 'a') as f:
-    	for prefix in current_prefix:
-    		f.write(prefix+'\n')
+        for prefix in current_prefix:
+            f.write(prefix+'\n')
     # print current_prefix
     print len(current_prefix)
